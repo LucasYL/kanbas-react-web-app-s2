f4c3a5e703e2d445d67bd90768cdb03cbdae2393
luyingcong617@gmail.com <luyingcong617@gmail.com>
2024-07-07 17:23:19 -0700

    fix

--- src/Labs/Lab1/Images.tsx
+++ src/Labs/Lab1/Images.tsx
@@ -7,13 +7,13 @@ export default function Images() {
   <img id="wd-starship"
     width="400px"
    src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
-   alt="Description of image 1"
+   alt="Starship in space"
   />
   <br />
   Loading a local image:
   <br />
   <img id="wd-teslabot" src="images/teslabot.jpg" height="200px" 
-   alt="Description of local image"/>
+   alt="Tesla bot"/>
 </div>
     );
 }
